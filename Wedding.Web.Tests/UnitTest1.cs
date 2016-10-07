909d0e9b0647ea645389016383e9bd738f850a5c
Studson <jstutson@gmail.com>
2016-10-06 21:46:46 -0500

    Web project

---
+++ Wedding.Web.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Wedding.Web.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
