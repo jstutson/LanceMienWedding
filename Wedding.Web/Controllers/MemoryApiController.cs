d1723ee7752e304fda9922f780407829918e2ce3
Studson <jstutson@gmail.com>
2016-10-11 20:51:52 -0500

    added first api controller and save method

---
+++ Wedding.Web/Controllers/MemoryApiController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Wedding.Web.Models.Memory;
+
+namespace Wedding.Web.Controllers
+{
+    [RoutePrefix("memory")]   // {domain}/memory/save
+    public class MemoryApiController : ApiController
+    {
+        [Route("save")]
+        [HttpPost]
+        public IHttpActionResult SaveMemory(MemorySaveRequest request)
+        {
+            // TODO: upload the image to our blob server
+            // TODO: update model with the URL for the image blob that we get back from saving to the blob server.
+            // If image save fails, return an error response
+            // else if successful then save the memory to our DB via entity framework
+            // along with the URL to our image.
+            
+            return Ok();
+        }
+    }
+}
