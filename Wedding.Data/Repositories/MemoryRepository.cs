a83e0657a6e082b280c7512ae6de901ca5080490
Studson <jstutson@gmail.com>
2016-12-03 14:51:20 -0600

    Add Unity, EF, and start repo pattern

---
+++ Wedding.Data/Repositories/MemoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wedding.Data.Repositories
+{
+    public class MemoryRepository : IMemoryRepository
+    {
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(object memory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(object memory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Wedding.Web/Controllers/MemoryApiController.cs
+++ Wedding.Web/Controllers/MemoryApiController.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.Web.Http;
 using Wedding.Web.Models.Memory;
+using Wedding.Data.Repositories;
 
 namespace Wedding.Web.Controllers
 {
     [RoutePrefix("memory")]   // {domain}/memory/save
     public class MemoryApiController : ApiController
     {
+        private IMemoryRepository memoryRepository;
+
+
+        //public MemoryApiController(IMemoryRepository memoryRepository)
+        //{
+        //    this.memoryRepository = memoryRepository;
+        //}
+
         [Route("save")]
         [HttpPost]
         public IHttpActionResult SaveMemory(MemorySaveRequest request)
@@ -16,6 +25,12 @@ namespace Wedding.Web.Controllers
             // If image save fails, return an error response
             // else if successful then save the memory to our DB via entity framework
             // along with the URL to our image.
+            var memoryEntity = new
+            {
+                Comment = request.Comment
+            };
+
+            //memoryRepository.Insert(memoryEntity);
             
             return Ok();
         }
